efaa5804f88c8a4f7bbe06377e77f1a031b159cf
jonathanmorais <jonathan.m.lucena@gmail.com>
2022-06-17 16:09:19 -0300

    api packages

--- t1d-api/main.go
+++ t1d-api/main.go
@@ -2,23 +2,51 @@ package main
 
 // Importing fmt
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jonathanmorais/endoenginnering/t1d-api/controllers/calculate"
 	"github.com/jonathanmorais/endoenginnering/t1d-api/controllers/healthcheck"
+	"github.com/jonathanmorais/endoenginnering/t1d-api/pkg/memory_cache"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"os"
+	"strconv"
+	"time"
 )
 
 // Calling main
 func main() {
 
-	// Creates a gin router with default middleware:
-	// logger and recovery (crash-free) middleware
-	router := gin.Default()
+	// Logger
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	if gin.IsDebugging() {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
 
+	log.Logger = log.Output(
+		zerolog.ConsoleWriter{
+			Out:     os.Stderr,
+			NoColor: false,
+		},
+	)
+
+	// use memory cache package
+	c := memory_cache.GetInstance()
+	// Readiness Probe Mock Config
+	probeTimeRaw := os.Getenv("READINESS_PROBE_MOCK_TIME_IN_SECONDS")
+	if probeTimeRaw == "" {
+		probeTimeRaw = "5"
+	}
+	probeTime, err := strconv.ParseUint(probeTimeRaw, 10, 64)
+	if err != nil {
+		fmt.Println("Environment variable READINESS_PROBE_MOCK_TIME_IN_SECONDS conversion error", err)
+	}
+	c.Set("readiness.ok", "false", time.Duration(probeTime)*time.Second)
+
+	// Creates a gin router with default middleware:
+	router := gin.New()
 	router.GET("/health", healthcheck.Ok)
 	router.POST("/calculate", calculate.Calculate)
 
-	// By default it serves on :8080 unless a
-	// PORT environment variable was defined.
 	router.Run()
-	//router.Run(":3000")
 }
