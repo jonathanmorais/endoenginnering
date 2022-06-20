d11306b21fa40d69791cb53de80cd38d89bf1082
jonathanmorais <jonathan.m.lucena@gmail.com>
2022-06-20 18:28:59 -0300

    first grpc

--- cr-service/main.go
+++ cr-service/main.go
@@ -1,3 +1,52 @@
 package main
 
-func main() {}
+import (
+	"github.com/jonathanmorais/endoenginnering/cr-service/pkg/logger"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"google.golang.org/grpc"
+	"net"
+	"os"
+)
+
+func main() {
+
+	logInternal := logger.Instance()
+
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	log.Logger = log.Output(
+		zerolog.ConsoleWriter{
+			Out:     os.Stderr,
+			NoColor: false,
+		},
+	)
+
+	lis, err := net.Listen("tcp", ":30000")
+	if err != nil {
+		log.Error().
+			Str("Error", err.Error()).
+			Msg("Failed to listen")
+	}
+
+	logInternal.Info().
+		Msg("Listener for carbohidra-service is created")
+
+	//	s := proteins.Server{}
+
+	grpcServer := grpc.NewServer(grpc.UnaryInterceptor(otelgrpc.UnaryServerInterceptor()),
+		grpc.StreamInterceptor(otelgrpc.StreamServerInterceptor()))
+
+	//	proteins.RegisterProteinsServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		logInternal.Error().
+			Str("Error", err.Error()).
+			Msg("Failed to server gRPC server")
+	}
+
+	logInternal.Info().
+		Msg("Server proteins-grpc-service is enabled")
+
+}
